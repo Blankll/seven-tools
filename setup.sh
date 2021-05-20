dcb3ac738f7d5a919d013f03239eb950bc7e62ff
Blankll <zilisheng1996@gmail.com>
2021-05-20 11:25:30 +0800

    move relateive dir

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ git clone https://github.com/Blankll/seven-vim.git
 
 cd seven-vim
 
-ln -s .$(pwd)/.vimrc ~/.vimrc
+ln -s $(pwd)/.vimrc ~/.vimrc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 

efeb0a114c8381daac76a10c848c79e8bc236cd0
Blankll <zilisheng1996@gmail.com>
2021-05-20 11:19:10 +0800

    move quit

--- setup.sh
+++ setup.sh
@@ -8,6 +8,5 @@ ln -s .$(pwd)/.vimrc ~/.vimrc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-vim -c ":PlugInstall | quit | quit"
+vim -c ":PlugInstall"
 
-vim

1933b920e3b70c37c75b213f3b74f78e9a21b22a
Blankll <zilisheng1996@gmail.com>
2021-05-22 16:22:51 +0800

    install vim plugin ignore loading error

--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ ln -s $(pwd)/.vimrc ~/.vimrc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-vim -c ":PlugInstall"
+vim -E -s -u "$HOME/.vimrc" +PlugInstall +qall
 

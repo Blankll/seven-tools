86ab5d0a64ac58bca145cda26ea213d1755970e3
Blankll <zilisheng1996@gmail.com>
2022-03-18 05:24:26 +0800

    config delta & tig

--- macos-setup.sh
+++ macos-setup.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
+# homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+# ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-brew install git
+
 brew install --cask iterm2
-brew install --cask alfred
+
+brew install git
 brew install docker
+brew install tig
+brew install git-delta
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+# install seven-tools
+bash <(curl -s https://raw.githubusercontent.com/Blankll/seven-tools/master/setup.sh)
+# install powerlevel10k
+brew install romkatv/powerlevel10k/powerlevel10k
+powerlevel_configed=$(cat ~/.zshrc | grep 'powerlevel10k.zsh-theme')
+if [[ -z $powerlevel_configed ]]; then
+    echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+fi
+
+brew install --cask alfred
 brew install --cask typora
 brew install --cask dbeaver-community
 brew install --cask postman
@@ -13,17 +29,7 @@ brew install --cask spacelauncher
 brew install --cask shiftit
 brew install --cask clipy
 
+brew install --cask neteasemusic
 brew install --cask youdaodict
 brew install --cask qq
 brew install --cask wechat
-brew install --cask neteasemusic
-if [[ ! -d "~/Documents/tools/" ]]; then
-    kdir ~/Documents/tools
-fi
-
-# install seven-tools
-bash <(curl -s https://raw.githubusercontent.com/Blankll/seven-tools/master/setup.sh)
-
-# install powerlevel10k
-brew install romkatv/powerlevel10k/powerlevel10k
-echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
--- setup.sh
+++ setup.sh
@@ -25,6 +25,14 @@ cd ~/Documents/tools
 
 ln -s $(pwd)/.vimrc ~/.vimrc
 
+# config git-delta
+delta_configed=$(cat ~/.gitconfig | grep 'delta')
+if [[ -z $delta_configed ]]; then
+    cat ./delta-gitconfig.txt >> ~/.gitconfig
+fi
+# config tig
+ln -s $(pwd)/.tigrc ~/.tigrc
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 vim -E -s -u "$HOME/.vimrc" +PlugInstall +qall
